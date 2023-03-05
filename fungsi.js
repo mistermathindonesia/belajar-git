3b6694bd908f5b30bf0177dcedf8fd0aaa41c896
Mister Math Indonesia <mistermathindonesia@gmail.com>
2023-03-06 04:30:16 +0700

    Revert "perkalian"
    This reverts commit b7f0805a548c3935c5e78dd81d6b04767f6ad3ae.

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1,2));
 const subtract = (a, b) => a - b;
 console.log(subtract(10,7))
 
-const multiply = (a, b) => a * b;
-console.log(multiply(10,7))
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(10,7))
 
 const divide = (a, b) => a / b;
 console.log(divide(50,10))
